c3d5aab2167443a1fa801d73eadcb235964f0af2
장효원 <hyo961015@naver.com>
2021-12-30 21:31:09 +0900

    init

---
+++ SwiftUISheetExample/SwiftUISheetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISheetExampleApp.swift
+//  SwiftUISheetExample
+//
+//  Created by 장효원 on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISheetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
